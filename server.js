8aff594536577fc3776d27e993174e737c6e48e2
suryadwd <surajdwivedi644@gmail.com>
2025-12-04 21:06:39 +0530

    matched the routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import 'dotenv/config.js';
 import express from 'express';
 import { connectDB } from './db/db.js';
 
-import { allRoutesInfo } from './controllers/allRoutesInfo.js';
+import allRoutesInfo  from './routes/allRoutesInfo.js';
 import fetchRoutes from './routes/fetchRoutes.js';
 import postRoutes from './routes/postRoutes.js';
 import userRoutes from './routes/userRoutes.js';
